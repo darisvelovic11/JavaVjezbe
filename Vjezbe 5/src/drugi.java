6450e72bd86b60613d4666aa00be74b969b86604
darisvelovic11 <daris.velovic11@gmail.com>
2025-10-23 15:30:05 +0200

    Add files via upload

---
+++ Vjezbe 5/src/drugi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class drugi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Unesite rijec: ");
+		String rijec = sc.nextLine().trim();
+		
+		rijec = rijec.replaceAll("\\s+", "" );
+		
+		
+		String obrnuto = new StringBuilder(rijec).reverse().toString();
+		
+		if(rijec.equalsIgnoreCase(obrnuto)) {
+			System.out.println("Rijec je palindrom.");
+		} else {
+			System.out.println("Rijec nije palindrom.");
+		}
+	}
+
+}
---
+++ Vjezbe 5/src/treci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class treci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Unesite rijec: ");
+		String rijec = sc.nextLine();
+		
+		boolean nadjeno = false;
+		
+		for(int i = 0; i < rijec.length()-1; i++) {
+			if(rijec.charAt(i) == rijec.charAt(i+1)) {
+				System.out.println("Prvi par ponovljenih slova je: " + rijec.charAt(i) + rijec.charAt(i+1));
+				nadjeno = true;
+				break;
+			}
+		
+		}
+		
+			
+		}
+	}
+
+
